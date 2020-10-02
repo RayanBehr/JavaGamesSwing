722ad38209427d076d9ef3d29fdac8578cf15f53
RayanBehr <rayan_behr@sluz.ch>
2020-10-03 00:23:09 +0200

    validation

--- Snake/src/com/zetcode/Board.java
+++ Snake/src/com/zetcode/Board.java
@@ -68,7 +68,7 @@ public class Board extends JPanel implements ActionListener {
         ImageIcon iid = new ImageIcon("src/resources/dot.png");
         ball = iid.getImage();
 
-        ImageIcon iia = new ImageIcon("src/resources/apple.png");
+        ImageIcon iia = new ImageIcon("src/resources/star.png");
         star = iia.getImage();
 
         ImageIcon iih = new ImageIcon("src/resources/head.png");
